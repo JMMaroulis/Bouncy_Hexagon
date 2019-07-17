950db11cb2c6ae834890fba43329c4f329781887
unknown <jamesmaroulis@gmail.com>
2019-07-17 22:51:18 +0100

    Basic pentatonic collision sounds

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,13 +7,29 @@ public class Player : MonoBehaviour
     public Camera mainCamera;
 
     private Rigidbody2D rigidBody2D;
-    private float forceMagnitude = 500f;    
+    private float forceMagnitude = 500f;
+
+    public AudioClip NoteC;
+    public AudioClip NoteA;
+    public AudioClip NoteG;
+    public AudioClip NoteE;
+    public AudioClip NoteD;
+    private List<AudioClip> Notes = new List<AudioClip>();
+
+    private AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
-        
+
+        Notes.Add(NoteC);
+        Notes.Add(NoteA);
+        Notes.Add(NoteG);
+        Notes.Add(NoteE);
+        Notes.Add(NoteD);
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,7 +40,6 @@ public class Player : MonoBehaviour
             MoveOnclick();
 
         }
-
     }
 
     void MoveOnclick()
@@ -35,9 +50,12 @@ public class Player : MonoBehaviour
         Debug.Log(mousePosition);
         Debug.Log(playerPosition);
 
-
         rigidBody2D.AddForce(relativePosition.normalized * forceMagnitude);
     }
 
-
+    void OnCollisionEnter2D()
+    {
+        int index = Mathf.RoundToInt(Random.value * 5);
+        audioSource.PlayOneShot(Notes[index]);
+    }
 }
