1c7186c5b20886437d27bdac6f0b31750dccb8df
unknown <jamesmaroulis@gmail.com>
2019-07-20 20:18:44 +0100

    Game boundary reset

---
+++ Assets/Scripts/CameraSquare.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSquare : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //GetComponent<Camera>().aspect = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ResetBoundary.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetBoundary : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerExit2D()
+    {
+        SceneManager.LoadScene("HexagonScene");
+    }
+}
