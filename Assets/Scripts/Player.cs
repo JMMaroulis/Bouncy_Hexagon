3428035da62876aa0791a466e3ce584de59b332a
unknown <jamesmaroulis@gmail.com>
2019-07-27 18:41:26 +0100

    Basic shadowcasting stuff working; still needs +- radian for proper effect

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Player : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class Player : MonoBehaviour
 
     private Rigidbody2D rigidBody2D;
     private float forceMagnitude = 500f;
+    public Material material;
 
     public AudioClip NoteC;
     public AudioClip NoteA;
@@ -21,6 +23,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Physics2D.queriesStartInColliders = false;
         rigidBody2D = GetComponent<Rigidbody2D>();
 
         Notes.Add(NoteC);
@@ -35,11 +38,13 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0))
         {
             MoveOnclick();
-
         }
+        RaycastTestThing();
+
+
     }
 
     void MoveOnclick()
@@ -47,8 +52,6 @@ public class Player : MonoBehaviour
         Vector2 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
         Vector2 playerPosition = new Vector2(transform.position.x, transform.position.y);
         Vector2 relativePosition = mousePosition - playerPosition;
-        Debug.Log(mousePosition);
-        Debug.Log(playerPosition);
 
         rigidBody2D.AddForce(relativePosition.normalized * forceMagnitude);
         //rigidBody2D.velocity = (relativePosition.normalized * 5f);
@@ -60,5 +63,140 @@ public class Player : MonoBehaviour
         audioSource.PlayOneShot(Notes[index]);
     }
 
- 
+    void RaycastTestThing()
+    {
+        //rotation quaternian for getting past points
+        //Quaternion extraRotation = new Quaternion(0, 0, degreeRotationExtra, 0);
+
+        //get all hexagon vertices
+        LineRenderer[] allHexagons = Object.FindObjectsOfType<LineRenderer>();
+        Vector3[][] allPoints = new Vector3[allHexagons.Length][];
+        
+        for (int i = 0; i < allHexagons.Length; i++)
+        {
+            allPoints[i] = new Vector3[6];
+            allHexagons[i].GetPositions(allPoints[i]);
+        }
+
+        //raycast to all vertices, TODO: and also +- 0.02 radians or so
+        List<RaycastHit2D> hitList = new List<RaycastHit2D>();
+
+        for (int i = 0; i < allPoints.Length; i++)
+        {
+            //get hex scale, hex rotation
+            Vector3 hexScale = allHexagons[i].transform.localScale;
+            Quaternion hexRotation = allHexagons[i].transform.rotation;
+
+            for (int j = 0; j < allPoints[i].Length; j++)
+            {
+                //rotate and scale to get from point definition to world space
+                Vector3 hexPointWorld = hexRotation * allPoints[i][j];
+                hexPointWorld = new Vector3(hexPointWorld.x * hexScale.x, hexPointWorld.y * hexScale.y, hexPointWorld.z * hexScale.z);
+                
+                //relative position to player, raycast, add to results
+                Vector3 direction = hexPointWorld - transform.position;
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, direction);
+                hitList.Add(hit);
+            }
+        }
+
+        //raycast to 4 corners (this should not be this much fucking work, it really shouldn't)
+        GameObject boundingBox = GameObject.FindGameObjectWithTag("GameBoundary");
+        EdgeCollider2D boundingBoxcollider = boundingBox.GetComponent<EdgeCollider2D>();
+        var bounds = boundingBoxcollider.bounds;
+        Vector2 centre = bounds.center;
+        float width = bounds.size.x;
+        float height = bounds.size.y;
+
+        Vector3 a = new Vector3(centre.x + (0.5f * width), centre.y + (0.5f * height), 0);
+        Vector3 b = new Vector3(centre.x - (0.5f * width), centre.y + (0.5f * height), 0);
+        Vector3 c = new Vector3(centre.x + (0.5f * width), centre.y - (0.5f * height), 0);
+        Vector3 d = new Vector3(centre.x - (0.5f * width), centre.y - (0.5f * height), 0);
+
+        hitList.Add(Physics2D.Raycast(transform.position, a - transform.position));
+        hitList.Add(Physics2D.Raycast(transform.position, b - transform.position));
+        hitList.Add(Physics2D.Raycast(transform.position, c - transform.position));
+        hitList.Add(Physics2D.Raycast(transform.position, d - transform.position));
+
+
+        //sort raycast hit position by angle from Y direction,
+        //then convert into vector3 array, prepend player position
+        List<RaycastHit2D> hitListSorted = hitList.OrderBy(vector => Vector3.SignedAngle(vector.point - (Vector2)transform.position, new Vector3(0, 1, 0), new Vector3(0, 0, 1))).ToList();
+        Vector3[] hitVectorsSorted = new Vector3[hitListSorted.Count + 1];
+        hitVectorsSorted[0] = transform.position;
+
+        for (int i = 0; i < hitVectorsSorted.Length-1; i++)
+        {
+            hitVectorsSorted[i+1] = hitListSorted[i].point;
+        }
+
+        //construct one biiiiig polygon out of the resulting raycast collision points
+        Mesh shadowOverlay = new Mesh();
+        shadowOverlay.vertices = hitVectorsSorted;
+        shadowOverlay.triangles = PolynomialTriangles(hitVectorsSorted.Length);
+        Debug.Log("Got There!");
+
+        Graphics.DrawMesh(shadowOverlay, Vector3.zero, Quaternion.identity, material, 20);
+        //DrawTestMesh();
+
+    }
+
+    //assumes that centre of polynomial is at index [0] of your vertex list
+    int[] PolynomialTriangles(int numVertices)
+    {
+        int[] triangles = new int[(numVertices - 1) * 3];
+        for (int i = 0, j = 0; j < triangles.Length-5; i+=1, j+=3)
+        {
+            triangles[j] = 0;
+            triangles[j + 1] = i + 1;
+            triangles[j + 2] = i + 2;
+        }
+        
+        triangles[triangles.Length - 3] = 0;
+        triangles[triangles.Length - 2] = numVertices - 1;
+        triangles[triangles.Length - 1] = 1;
+
+        return triangles;
+    }
+
+    void DrawTestMesh()
+    {
+        //raycast to 4 corners (this should not be this much fucking work, it really shouldn't)
+        GameObject boundingBox = GameObject.FindGameObjectWithTag("GameBoundary");
+        EdgeCollider2D boundingBoxcollider = boundingBox.GetComponent<EdgeCollider2D>();
+        var bounds = boundingBoxcollider.bounds;
+        Vector2 centre = bounds.center;
+        float width = bounds.size.x;
+        float height = bounds.size.y;
+
+        Vector3 a = new Vector3(centre.x + (0.5f * width), centre.y + (0.5f * height), 0);
+        Vector3 b = new Vector3(centre.x + (0.5f * width), centre.y - (0.5f * height), 0);
+        Vector3 c = new Vector3(centre.x - (0.5f * width), centre.y - (0.5f * height), 0);
+        Vector3 d = new Vector3(centre.x - (0.5f * width), centre.y + (0.5f * height), 0);
+
+        List<RaycastHit2D> hitList = new List<RaycastHit2D>();
+        hitList.Add(Physics2D.Raycast(transform.position, a - transform.position));
+        hitList.Add(Physics2D.Raycast(transform.position, b - transform.position));
+        hitList.Add(Physics2D.Raycast(transform.position, c - transform.position));
+        hitList.Add(Physics2D.Raycast(transform.position, d - transform.position));
+
+        List<RaycastHit2D> hitListSorted = hitList.OrderBy(vector => Vector3.SignedAngle(vector.point - (Vector2)transform.position, new Vector3(0, 1, 0), new Vector3(0, 0, 1))).ToList();
+        Vector3[] hitVectorsSorted = new Vector3[hitListSorted.Count + 1];
+        hitVectorsSorted[0] = transform.position;
+
+        for (int i = 0; i < hitVectorsSorted.Length - 1; i++)
+        {
+            hitVectorsSorted[i + 1] = hitListSorted[i].point;
+        }
+
+        Mesh testMesh = new Mesh();
+        //testMesh.vertices = new Vector3[] { transform.position, a, b, c, d };
+        testMesh.vertices = hitVectorsSorted;
+
+
+        testMesh.triangles = new int[] { 0,1,2, 0,2,3, 0,3,4 };
+        Graphics.DrawMesh(testMesh, Vector3.zero, Quaternion.identity, material, 30);
+    }
+
+
 }
